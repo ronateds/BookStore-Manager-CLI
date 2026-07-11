5c5f4d99a85c60505f2cd4fac27edd350299af31
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-07-11 19:10:54 -0300

    feat: menu de autores

---
+++ src/menus/autorMenu.ts
@@ -0,0 +1,49 @@
+import { select, Separator } from "@inquirer/i18n/pt";
+import { header } from "../utils/formatters";
+import { menuPrincipal } from "./mainMenu";
+
+export async function menuAutores(): Promise<void> {
+    header('Menu Autores')
+
+    const opcao = await select({
+        message: 'Escolha uma opção',
+        choices: [
+            new Separator(),
+            {
+                name: '1  Cadastrar autor',
+                value: '1',
+            },
+            {
+                name: '2  Listar autores',
+                value: '2',
+            },
+            {
+                name: '3  Consultar autor por id',
+                value: '3',
+            },
+            {
+                name: '4  Atualizar autor',
+                value: '4',
+            },
+            {
+                name: '5  Remover autor',
+                value: '5',
+            },
+            {
+                name: '<  Voltar',
+                value: '0',
+            },
+        ],
+    });
+
+    switch (opcao) {
+        case '1':
+            console.log('cadastrar');
+            break;
+        case '0':
+            menuPrincipal();
+            break;
+        default:
+            console.log('\nOpção inválida. Tente novamente.');
+    }
+}
