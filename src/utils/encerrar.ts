ea073e761238faa2761d16faf729b4196d8adfb4
Ronatê dos Santos <38088684+ronateds@users.noreply.github.com>
2026-07-12 07:50:44 -0300

    feat: limpar terminal ao encerrar

--- src/utils/encerrar.ts
+++ src/utils/encerrar.ts
@@ -1,6 +1,7 @@
 import { pool } from "../database/connection";
 
 export async function encerrrar() {
+    console.clear();
     console.log('\nEncerrando a aplicação. Até logo!\n');
     await pool.end();
     process.exit()
