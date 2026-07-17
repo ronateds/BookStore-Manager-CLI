93e1b9808c972d25059ff39d8470b8a8bd8d4213
willian piccinin <picci1522@gmail.com>
2026-07-17 16:25:51 +0000

    feat: Listar  no  ClienteController

--- src/controllers/ClienteController.ts
+++ src/controllers/ClienteController.ts
@@ -1,9 +1,26 @@
+import { ICliente } from "../models/Cliente";
 import { ClienteService } from "../services/ClienteService";
+import { listarCliente, listarTodosClientes } from "../utils/formatters";
+import { tratarErro } from "../utils/tratarErro";
+
 
 export class ClienteController {
     private clienteService = new ClienteService();
 
-    // TODO listar
+    async listar(): Promise<void> {
+        try {
+            const clientes: ICliente[] = await this.clienteService.listar();
+
+            clientes.length ?
+                listarTodosClientes(clientes) :
+                console.log('Nenhum cliente cadastrado.\n');
+
+            return;
+        } catch (error) {
+            tratarErro(error);
+            return;
+        }
+    }
 
     // TODO cadastrar
 
