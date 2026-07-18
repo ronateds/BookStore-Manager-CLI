f701543e66cb8ea6bf2e5b7bf251cba138bba317
willian piccinin <picci1522@gmail.com>
2026-07-18 01:20:07 +0000

    feat: implementar método atualizar com valores padrão no controlador

--- src/controllers/EmprestimoController.ts
+++ src/controllers/EmprestimoController.ts
@@ -50,7 +50,38 @@ export class EmprestimoController {
         }
     }
 
-    // TODO atualizar
+    async atualizar(): Promise<void> {
+        try {
+            // Procura emprestimo por id primeiro
+            const id = Number(await input({ message: 'Informe o id do emprestimo: ' }));
+            const emprestimo = await this.emprestimoService.buscarPorId(id);
+            if (!emprestimo) throw new AppError(`Não foi encontrado emprestimo com id: ${ id }`);
+
+            // Pede inputs ao usuario definindo como padrão os atributos do emprestimo existente
+            const cliente_id = Number(await input({ message: "ID do cliente: ", default: String(emprestimo.cliente_id) }));
+            const livro_id = Number(await input({ message: "ID do livro: ", default: String(emprestimo.livro_id) }));
+            const data_emprestimo = new Date(await input({ message: "Data de empréstimo: ", default: String(emprestimo.data_emprestimo) }));
+            const data_devolucao = new Date(await input({ message: "Data de empréstimo: ", default: String(emprestimo.data_devolucao) }));
+
+
+            // Atualiza o emprestimo
+            const emprestimoAtualizado = await this.emprestimoService.atualizar(id, {
+                id,
+                cliente_id,
+                livro_id,
+                data_emprestimo,
+                data_devolucao
+            });
+
+            if (!emprestimoAtualizado) throw new AppError('Livro não foi atualizado.');
+
+            console.log(`\nEmprestimo atualizado com sucesso!\n`);
+            listarEmprestimo(emprestimoAtualizado);
+            console.log();
+        } catch (error) {
+            tratarErro(error);
+        }
+    }
 
     // TODO remover
 }
