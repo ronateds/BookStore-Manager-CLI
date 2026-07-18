e5e1372754753bf62d3e5a767763001c1b3cde4a
willian piccinin <picci1522@gmail.com>
2026-07-18 01:13:53 +0000

    feat: implementar método listar no controlador de empréstimos

--- src/controllers/EmprestimoController.ts
+++ src/controllers/EmprestimoController.ts
@@ -1,9 +1,27 @@
+import { input } from "@inquirer/i18n";
 import { EmprestimoService } from "../services/EmprestimoService";
+import { tratarErro } from "../utils/tratarErro";
+import { IEmprestimo } from "../models/Emprestimo";
+import { listarEmprestimo, listarTodosEmprestimos } from "../utils/formatters";
+import { AppError } from "../utils/AppError";
 
 export class EmprestimoController {
     private emprestimoService = new EmprestimoService();
 
-    // TODO listar
+    async listar(): Promise<void> {
+        try {
+            const emprestimos: IEmprestimo[] = await this.emprestimoService.listar();
+
+            emprestimos.length ?
+                listarTodosEmprestimos(emprestimos) :
+                console.log('Nenhum emprestimo cadastrado.\n');
+
+            return;
+        } catch (error) {
+            tratarErro(error);
+            return;
+        }
+    }
 
     // TODO cadastrar
 
