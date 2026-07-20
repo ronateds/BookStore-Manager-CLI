b90a834011565c4586bcd6c4f25ef0c6046dacff
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-07-20 19:35:30 -0300

    fix: importação dando problema

--- src/menus/mainMenu.ts
+++ src/menus/mainMenu.ts
@@ -1,7 +1,7 @@
 import { encerrrar } from '../utils/encerrar';
 import { escolherDoMenu, header, opcaoMenu } from '../utils/menuHelper';
 import { menuAutores } from './autorMenu';
-import { menuLivros } from './LivroMenu';
+import { menuLivros } from './livroMenu';
 import { clienteMenu } from './clienteMenu';
 import { emprestimoMenu } from './emprestimoMenu';
 import { relatoriosMenu } from './relatorioMenu';
